568785a2835ff322462b8f449c4f847f0c30c7ff
Romulo-Moraes <romuloperes6@gmail.com>
2022-02-07 16:52:03 -0300

    Added methods to set encrypt, header updates

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,29 +4,22 @@
 #include <readline/readline.h>
 #include "./../zipCraft/zipCraftHeader.hpp"
 #include <string.h>
+#include <zip.h>
 
 using namespace std;
 
 int main(){
-    char *userInput;
-    cout << "We need find the zip file password, you can do it?" << endl;
-
     zipCrafter crafter("package.zip");
 
-    crafter.openZip();
+    crafter.createZip();
 
-    cout << crafter.checkIfEncrypted() << endl;
+    crafter.writeFile("exmaple.txt","hello!",6);
 
-    while(true){
-        userInput = readline("Type some password: ");
-        char buffer[3];
-        if(crafter.readEncryptedFileByIndex(0,userInput,2,buffer)){
-            cout << "Well done, you found the password!" << endl;
-            cout << buffer;
-            exit(0);
-        }
-        else{
-            cout << "Wrong password, try again" << endl << endl;
-        }
-    };
+    cout << crafter.checkIfExists("example.txt");
+
+    crafter.setPasswordToFile("example.txt",AES_256_ENCRYPTION,":)");
+
+    crafter.closeZip();
+    
+
 };
